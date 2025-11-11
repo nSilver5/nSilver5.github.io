14784189be37913f6fdaa7e9da3a2ed3ec5bf52a
nSilver5 <noah_silverman@brown.edu>
2025-11-11 22:26:26 +0000

    Deploying to gh-pages from @ nSilver5/nSilver5.github.io@304d7ef00f575a6b01bae694308755ef0e102bca 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "nSilver5/nsliver5.github.io",
+    "data-repo": "nSilver5/nSilver5.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
