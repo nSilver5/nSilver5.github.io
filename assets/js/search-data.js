df914be86a413b5da6f762c28dff3e2312354a53
nSilver5 <noah_silverman@brown.edu>
2025-12-15 20:00:22 +0000

    Deploying to gh-pages from @ nSilver5/nSilver5.github.io@53cc970d962fe313010b3d893c21dfb1076720a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,8 +10,8 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "Relevant Engineering.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
